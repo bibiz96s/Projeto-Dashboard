877e5e64ad268dab30ef9280271d24466f4e977e
Tu Nombre <tu@email.com>
2024-09-15 11:15:59 -0300

    atualizações

--- app.py
+++ app.py
@@ -4,13 +4,10 @@ import pandas as pd
 
 app = Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
+
 df = pd.read_csv("EvasãoSérieHistórica2024.csv", encoding='latin1',delimiter=';')
 
 opcoes = df.columns.tolist()
-#Criando o Grafico
-#fig = px.bar(df, x="Ano", y="Vinculados", title="Dados sobre os alunos da Engenharia de Computacao")
 
 
 app.layout = html.Div(children=[
@@ -21,26 +18,47 @@ app.layout = html.Div(children=[
     dcc.Dropdown(opcoes, value= 'Vinculados', id='y'),
     
     dcc.Graph(
-        id='grafico-ecomp'
+        id='grafico-barras'
+    ),
+    
+    dcc.Graph(
+        id='grafico-linhas'
     )
+
 ])
 
 
 @callback(
-    Output('grafico-ecomp', 'figure'),
+    Output('grafico-barras', 'figure'),
     Input('color', 'value'),
     Input('y', 'value')
 )
-def update_output(value_color,value_y):
+def update_bar(value_color,value_y):
 
     
-    fig = px.bar()
+    fig_bar = px.bar()
 
     for coluna in value_color:
-        print(coluna)
-        fig.add_trace(px.bar(df, x="Ano", y=value_y,color=coluna, title=f"Dados sobre {coluna} e {value_y}").data[0],)
+        
+        fig_bar.add_trace(px.bar(df, x="Ano", y=value_y,color=coluna, title=f"Dados sobre {coluna} e {value_y}").data[0],)
 
-    return fig
+    return fig_bar
+
+@callback(
+    Output('grafico-linhas', 'figure'),
+    Input('color', 'value'),
+    Input('y', 'value')
+)
+def update_lines(value_color,value_y):
+
+    
+    fig_lines = px.bar()
+
+    for coluna in value_color:
+        
+        fig_lines.add_trace(px.line(df, x="Ano", y=value_y,color=coluna, title=f"Dados sobre {coluna} e {value_y}").data[0],)
+
+    return fig_lines
 
 
 if __name__ == '__main__':
